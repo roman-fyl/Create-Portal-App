0705f8d30241a516c56e9c3cf35ab67fbe581f49
Roman Fylypiuk <155104150+roman-fyl@users.noreply.github.com>
2024-01-03 18:59:50 -0600

    upd project

---
+++ my-app/src/components/.Layout/Layout.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react';
+
+import "./Layout.css"
+
+const Layout = () => {
+ return (
+    <div>
+    <h2>Layout</h2>
+</div>
+ )
+};
+
+export default Layout;
